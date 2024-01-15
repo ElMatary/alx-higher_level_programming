497fc30c935b2202d94b345e5347b991b60c78b4
ElMatary <mahmoudelmatary724@yahoo.com>
2024-01-15 06:12:12 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-'''module for Base class'''
+'''Module for Base class.'''
+from json import dumps, loads
 
 
 class Base:
-    '''Base class'''
+    '''A representation of the base of our OOP hierarchy.'''
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''constructor'''
+        '''Constructor.'''
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
