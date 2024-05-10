e531558bea813aae47618bea13dedaf5caf390a9
ElMatary <mahmoudelmatary724@yahoo.com>
2024-05-10 10:08:44 -0700

    update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        the_page = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
